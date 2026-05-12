ca11c8a2269df3d87d7de080fe8ba301a7d6e6f9
heo <admin@admin.com>
2026-05-12 22:17:45 +0900

    commit

--- lib/register/yolo_register_phone.dart
+++ lib/register/yolo_register_phone.dart
@@ -36,6 +36,58 @@ class _RegisterPhonePagetate extends State<RegisterPhonePage> {
   final middleController = TextEditingController();
   final lastController = TextEditingController();
 
+  String selectedGender = "남성";
+  Widget customRadio(String gender) {
+    final bool isSelected = selectedGender == gender;
+
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedGender = gender;
+        });
+      },
+
+      child: Row(
+        children: [
+          SizedBox(
+            width: 24,
+            height: 24,
+
+            child: Stack(
+              alignment: Alignment.center,
+
+              children: [
+                // 기본 원
+                Image.asset(
+                  "assets/img/radio_button.png",
+                  width: 24,
+                  height: 24,
+                ),
+
+                // 선택된 경우 점 표시
+                if (isSelected)
+                  Image.asset(
+                    "assets/img/radio_button_dot.png",
+                    width: 12,
+                    height: 12,
+                  ),
+              ],
+            ),
+          ),
+
+          const SizedBox(width: 8),
+
+          Text(
+            gender,
+            style: const TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -227,27 +279,149 @@ class _RegisterPhonePagetate extends State<RegisterPhonePage> {
               ),
             ),
 
-            SizedBox(height: 16,),
-            Text("이메일", style: TextStyle(fontSize: 15)),
-            TextField(
-              decoration: InputDecoration(
-                hintText: "이메일을 입력해 주세요",
-                hintStyle: TextStyle(
-                  fontSize: 15, // 15sp
-                  color: Color(0xFFABABAB), // #333333
-                ),
-                contentPadding: EdgeInsets.all(10),
-                // 안쪽 패딩 10
-                filled: true,
-                fillColor: Colors.white,
+            SizedBox(height: 10,),
+            Text("지역(사는곳)", style: TextStyle(fontSize: 15)),
+            SizedBox(height: 6,),
+            Row(
+              children: [
+                Expanded(child:  DropdownButtonFormField<String>(
+                  //value: selectedYear,
+                  decoration: InputDecoration(
+                    hintText: "시/도",
+                    filled: true,
+                    fillColor: Colors.white,
+                    contentPadding: EdgeInsets.all(10),
 
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey),
+                    // TextField 느낌 테두리
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+
+                  // 🔽 아래 화살표 (기본 제공)
+                  icon: Icon(Icons.arrow_drop_down, color: Colors.grey),
+                  isExpanded: true, // 텍스트 꽉 차게
+                  items: years.map((y) {
+                    return DropdownMenuItem(
+                      value: y,
+                      child: Text(y),
+                    );
+                  }).toList(),
+
+                  onChanged: (value) {
+                    setState(() {
+                      selectedYear = value;
+                    });
+                  },
+                )),
+                SizedBox(width: 8),
+                Expanded(child:  DropdownButtonFormField<String>(
+                  //value: selectedYear,
+                  decoration: InputDecoration(
+                    hintText: "구/군",
+                    filled: true,
+                    fillColor: Colors.white,
+                    contentPadding: EdgeInsets.all(10),
+
+                    // TextField 느낌 테두리
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+
+                  // 🔽 아래 화살표 (기본 제공)
+                  icon: Icon(Icons.arrow_drop_down, color: Colors.grey),
+                  isExpanded: true, // 텍스트 꽉 차게
+                  items: years.map((y) {
+                    return DropdownMenuItem(
+                      value: y,
+                      child: Text(y),
+                    );
+                  }).toList(),
+
+                  onChanged: (value) {
+                    setState(() {
+                      selectedYear = value;
+                    });
+                  },
+                ))
+              ],
+            ),
+            SizedBox(height: 10,),
+
+
+            // Text("성별", style: TextStyle(fontSize: 15)),
+            // SizedBox(height: 6,),
+            // Row(
+            //   children: [
+            //     customRadio("남성"),
+            //     const SizedBox(width: 30),
+            //     customRadio("여성"),
+            //   ],
+            // ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // 성별
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "성별",
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          const SizedBox(height: 6),
+                          Row(
+                            children: [
+                              customRadio("남성"),
+                              const SizedBox(width: 30),
+                              customRadio("여성"),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    const SizedBox(width: 30),
+                    // 결혼 여부
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "결혼 여부",
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          const SizedBox(height: 6),
+                          Row(
+                            children: [
+                              customRadio("기혼"),
+                              const SizedBox(width: 30),
+                              customRadio("미혼"),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey),
-                ),
-              ),
+              ],
             ),
 
           ],
